c32272925f245e857a2f7b354d32715636df8191
201524495 <49744580+201524495@users.noreply.github.com>
2021-02-16 21:06:04 +0900

    Register.php에서 null값이 들어온 경우에 fail출력 값이 잘 들어오면 INSERT및 success 출력

--- 로그인/Register.php
+++ 로그인/Register.php
@@ -9,9 +9,9 @@ include_once('./password.php');
 // id와 암호화한 PW를 DB에 저장
     $hash = password_hash($userPassword, PASSWORD_DEFAULT);  // 비밀번호 암호화
 
-    $statement = mq("INSERT INTO USER VALUES ('$userID', '$userPassword', '$userName', '$userAge', '$hash')");
-
-    if(0) { // statement가 true가 되면 ( 입력값이 잘 들어온 경우 )
+//?????????//
+    if($userID != " ") { // statement가 true가 되면 ( 입력값이 잘 들어온 경우 )
+      $statement = mq("INSERT INTO USER VALUES ('$userID', '$userPassword', '$userName', '$userAge', '$hash')");
       echo 'success'; // *** NULL값 못받는 것도 해야함
     }
     else {// statement가 false가 되면 ( 입력값이 잘 안들어온 경우 )
--- 로그인/test.php
+++ 로그인/test.php
@@ -2,15 +2,18 @@
 include_once('./new_Login/db.php');
 include_once('./password.php');
 //MainActivity에서 DB의 내용을 모두 출력하고 싶다.
-$userID = $_POST["userID"]; //앱에서 입력받고
-$userpasswd = $_POST["userPassword"]; //앱에서 앱력받고
+$userID = $_GET["userID"]; //앱에서 입력받고
+$userpasswd = $_GET["userPassword"]; //앱에서 앱력받고
 
 //DB에 있는 userID와 입력받은 userID가 같은 레코드 select
 $sql = mq("SELECT * FROM USER WHERE userID='$userID'");
 $result = $sql->fetch_assoc();
 
-if() {//널 값이 아니면
-  echo 'success';
+if(strcmp($result['userID'], $userID) == 0) {//널 값이 아니면
+//  $result2json = array()
+  echo json_encode($result);//값이 주루룩 나옴
+  echo "\n";
+  echo $result['userID'];//userID 값만 나옴
 }
 else {//널 값이면
   echo 'fail';
